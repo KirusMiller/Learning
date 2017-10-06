ab2aa75fe8c0b1920340c4ac27f688783a558437
Shelly-ПК\Shelly <kirillstorkmelnikov@gmail.com>
2017-10-06 20:39:23 +0300

    purge

--- src/SoloClass/AnimalMain.java
+++
@@ -1,8 +0,0 @@
-package SoloClass;
-
-public class AnimalMain {
-    public static void main(String[] args) {
-        Animal dog = new Animal();//создан новый объект dog класса Animal
-        dog.bark();//вызов метода bark используя имя объекта и точку
-    }
-}
